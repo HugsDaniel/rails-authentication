13e9b364bc7bbf4e5596bbb82114abdc69d67ecf
Hugo <daniel.hugo@protonmail.com>
2022-11-21 10:40:18 +0100

    Devise setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
+    # @reservations = current_user.reservations
+  end
+
+  def contact
   end
 end
